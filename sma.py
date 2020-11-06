38c727163af24bf9482583f0ebbfcdd56778f4db
minhasnu <minhas.naheem.in@gmail.com>
2020-11-05 16:04:48 -0800

    sma basic need to work in sma window and interval

---
+++ sma.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.techindicators import TechIndicators
+import matplotlib.pyplot as plt 
+
+api_key = 'HQJMSKJUDBOQB04I'
+interval = '15min'
+ts = TimeSeries(key=api_key, output_format = 'pandas')
+data_ts, meta_data_ts = ts.get_intraday(symbol = 'MSFT', interval = interval, outputsize='full')
+
+
+period = 100
+ti = TechIndicators(key=api_key, output_format = 'pandas')
+data_ti, meta_data_ti = ti.get_sma(symbol = 'MSFT', interval = interval, time_period = period, series_type='close')
+
+df1 = data_ti
+df2 = data_ts['4. close'].iloc[period-1 : :]
+
+df2.index = df1.index
+total_df = pd.concat([df1,df2] , axis = 1)
+print(total_df)
+
+total_df.plot()
+plt.show()
